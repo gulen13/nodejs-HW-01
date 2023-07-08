5f451d1e080327a1f00185ee02d89f593de8f501
gulen13 <vitaliy.gulen@gmail.com>
2023-07-08 16:26:15 +0300

    HW is done

---
+++ README.md
@@ -0,0 +1,4 @@
+Отримуємо і виводимо весь список контактів у вигляді таблиці - https://monosnap.com/file/TfXRDIqHUwBS24WUzWijmXHct0WrfT
+Отримуємо контакт по id - https://monosnap.com/file/BbcgyxBnnszLP1Ghz9B3JxYAr0UlY0
+Додаємо контакт - https://monosnap.com/file/RtNgAWVJ9IOdlar0fkVZnEptF2xtpy
+Видаляємо контакт - https://monosnap.com/file/YpZ1f8zJrldt3IFiHjYwDezCZ3B54F
---
+++ index.js
@@ -0,0 +1,28 @@
+const { listContacts,
+  getContactById,
+  addContact,
+  removeContact
+} = require("./contacts.js");
+
+const argv = require('yargs').argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const allContacts = await listContacts();
+      return console.table(allContacts);
+    case 'get':
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+    case 'add':
+      const newContact = await addContact({ name, email, phone });
+      return console.log(newContact);
+    case 'remove':
+      const deletedContact = await removeContact(id);
+      return console.log(deletedContact);
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
